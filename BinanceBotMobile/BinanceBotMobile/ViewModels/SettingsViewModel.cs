1343ee522015f6c13de16a2a83855fb75a5a4495
Michael Delpach <McoreD@users.noreply.github.com>
2020-07-05 07:56:27 +0800

    SetProperty start

--- BinanceBotMobile/BinanceBotMobile/ViewModels/SettingsViewModel.cs
+++ BinanceBotMobile/BinanceBotMobile/ViewModels/SettingsViewModel.cs
@@ -12,13 +12,13 @@ namespace BinanceBot
         public override string APIKey
         {
             get => Preferences.Get(nameof(APIKey), "");
-            set { SetPreference(nameof(APIKey), value); }
+            set => SetPreference(nameof(APIKey), value);
         }
 
         public override string SecretKey
         {
             get => Preferences.Get(nameof(SecretKey), "");
-            set { SetPreference(nameof(SecretKey), value); }
+            set => SetPreference(nameof(SecretKey), value);
         }
 
         // Futures
@@ -26,7 +26,7 @@ namespace BinanceBot
         public override double FuturesSafetyFactor
         {
             get => Preferences.Get(nameof(FuturesSafetyFactor), 15.0);
-            set { SetPreference(nameof(FuturesSafetyFactor), value); }
+            set => SetPreference(nameof(FuturesSafetyFactor), value);
         }
 
         public List<string> TakeProfitModes
@@ -46,31 +46,31 @@ namespace BinanceBot
         public override bool IsAutoAdjustShortAboveAndLongBelow
         {
             get => Preferences.Get(nameof(IsAutoAdjustShortAboveAndLongBelow), false);
-            set { SetPreference(nameof(IsAutoAdjustShortAboveAndLongBelow), value); }
+            set => SetPreference(nameof(IsAutoAdjustShortAboveAndLongBelow), value);
         }
 
         public override double ShortAbove
         {
             get => Preferences.Get(nameof(ShortAbove), 9990.0);
-            set { SetPreference(nameof(ShortAbove), value); }
+            set => SetPreference(nameof(ShortAbove), value);
         }
 
         public override double LongBelow
         {
             get => Preferences.Get(nameof(LongBelow), 9490.0);
-            set { SetPreference(nameof(LongBelow), value); }
+            set => SetPreference(nameof(LongBelow), value);
         }
 
         public override bool IsAutoAdjustTargetProfit
         {
             get => Preferences.Get(nameof(IsAutoAdjustTargetProfit), false);
-            set { SetPreference(nameof(IsAutoAdjustTargetProfit), value); }
+            set => SetPreference(nameof(IsAutoAdjustTargetProfit), value);
         }
 
         public override double FuturesProfitTarget
         {
             get => Preferences.Get(nameof(FuturesProfitTarget), 0.0);
-            set { SetPreference(nameof(FuturesProfitTarget), value); }
+            set => SetPreference(nameof(FuturesProfitTarget), value);
         }
     }
 }
